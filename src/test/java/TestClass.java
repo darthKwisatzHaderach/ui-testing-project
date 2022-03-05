16b2b779ac8bb250104c8f6b5020979f85090ab6
Dmitriy Tonkushin <datonkushin@gmail.com>
2022-03-05 10:33:57 +0500

    Added Selenium and WebDriverManager dependencies. Added simple test class.

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,34 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void firstTest() {
+        driver.navigate().to("https://dins.ru/");
+    }
+}
